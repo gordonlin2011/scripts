34056081d3d8f308119fc6d6345f38980f28c578
gordonlin2011 <64232078+gordonlin2011@users.noreply.github.com>
2021-03-07 16:08:29 -0500

    Create pritunl.sh

---
+++ pritunl.sh
@@ -0,0 +1,20 @@
+echo This Script will install and give the username and password to a pritunl machine
+
+yum update -y
+sudo tee /etc/yum.repos.d/pritunl.repo << EOF
+[pritunl]
+name=Pritunl Repository
+baseurl=https://repo.pritunl.com/stable/yum/centos/7/
+gpgcheck=1
+enabled=1
+EOF
+
+sudo rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+gpg --armor --export 7568D9BB55FF9E5287D586017AE645C0CF8E292A > key.tmp; sudo rpm --import key.tmp; rm -f key.tmp
+systemctl disable firewalld
+systemctl stop firewalld
+pritunl setup-key
+echo Enter the setup key above to set up pritunl
+pritunl default-password
+echo Default Password Above

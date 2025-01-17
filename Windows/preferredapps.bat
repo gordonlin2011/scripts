37577939572121b6b727ce41cb3412d568fca9cd
gordonlin2011 <64232078+gordonlin2011@users.noreply.github.com>
2025-01-16 23:35:26 -0500

    added install script for personal use

---
+++ Windows/preferredapps.bat
@@ -0,0 +1,3 @@
+winget install Chocolatey.Chocolatey Brave.Brave Git.Git Valve.Steam Microsoft.VisualStudioCode OpenJS.NodeJS.LTS VideoLAN.VLC 7zip.7zip Notepad++.Notepad++ IDRIX.VeraCrypt TheDocumentFoundation.LibreOffice Nvidia.GeForceNow
+choco flashpoint-infinity
+wsl --install
